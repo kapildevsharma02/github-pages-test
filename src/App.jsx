b17412eaa04e382ce2c6d440f0b602111025c43b
Sharma <kdevsharma012@gmail.com>
2025-02-13 20:05:00 +0530

    Adding the paragraph tag and introduced myself

--- src/App.jsx
+++ src/App.jsx
@@ -17,19 +17,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React tutorials</h1>
-      <h1>By</h1>
-      <h1>Kapil Dev Sharma</h1>
+      <h1>By CodeHacks</h1>
+      <h1>Hi I am Kapil Dev Sharma</h1>
+      <p>Thank you for visiting my portfolio...</p>
+      <p>I am a passionate and self-motivated software developer with a B.Tech in Computer Science and Engineering from GL Bajaj Institute of Technology and Management, Noida. As a self-taught programmer, I have honed my skills in various programming languages and technologies through personal projects and continuous learning.</p>
+      <p>Relevant Coursework: Data Structures, Algorithms, Database Management Systems, Operating Systems, Software Engineering, Design Principles and Migration work</p>
+      <p>- **Programming Languages**: Python, Java, JavaScript, C++
+    - **Web Development**: HTML, CSS, React, Node.js
+    - **Databases**: MySQL, MongoDB
+    - **Tools**: Git, GitHub, Docker, Jenkins
+    - **Other**: Problem-solving, Debugging, Version Control</p>
+      <p></p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
